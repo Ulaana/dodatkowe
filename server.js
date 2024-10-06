31bef9276ffd58206df977cdb2cf3796687d7d15
Ulaana <157713810+Ulaana@users.noreply.github.com>
2024-10-06 19:05:41 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,20 @@
 import express from 'express';
 import fetch from 'node-fetch';
-import path from 'path'; // To resolve paths
-import { fileURLToPath } from 'url'; // Needed for __dirname in ESModules
+import path from 'path';
+import { fileURLToPath } from 'url'; 
 
 const app = express();
 const PORT = 3000;
 
-// __dirname workaround for ESModules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Obsługa plików statycznych z folderu 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Trasa główna serwująca plik HTMLn
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Trasa API do pobierania danych
 app.get('/api/data', async (req, res) => {
   try {
     const operator = await fetch('https://eipa.udt.gov.pl/reader/export-data/operator/85ac1231b31b7533ce1e332123706921').then(res => res.json());
